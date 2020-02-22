9e0871c18f7f2f5038e3c6ddde7c53d62b7f3999
ThisUsernameIsCool <grantreddickgames@gmail.com>
2020-02-22 11:24:53 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,13 +6,13 @@ import platform
 opSys = platform.system()
 
 if opSys == 'Windows':
-    print('...')
-    os.system('pause')
+
     
 elif opSys == 'Darwin':
     print('...')
 
 elif opSys == 'Linux':
     print('OS NOT SUPPORTED; VIRUS ABORTED')
+    
 else:
     print('You are Lucky')
