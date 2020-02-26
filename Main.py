a71b39b90f34a6e529193b0fe4c7c551cdeee123
ThisUsernameIsCool <grantreddickgames@gmail.com>
2020-02-25 22:40:10 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,21 +1,43 @@
-import os
-import time
+##import os
+##import time
+##import socket
+##import platform
+##
+##opSys = platform.system()
+##
+##if opSys == 'Windows':
+##    print('Windows')
+##    os.system('pause')
+##    
+##elif opSys == 'Darwin':
+##    print('Mac')
+##    read -n1 -r -p "Press any key to continue..." key
+##
+##elif opSys == 'Linux':
+##    print('Linux')
+##    
+##else:
+##    print('You are Lucky')
+
 import socket
-import platform
 
-opSys = platform.system()
+serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-if opSys == 'Windows':
-    print('Windows')
-    os.system('pause')
-    
-elif opSys == 'Darwin':
-    print('Mac')
-    read -n1 -r -p "Press any key to continue..." key
+serv.bind(('0.0.0.0', 8080))
+serv.listen(5)
 
-elif opSys == 'Linux':
-    print('Linux')
-    
-else:
-    print('You are Lucky')
+while True:
+    conn, addr = serv.accept()
+    from_client = ''
+
+    while True:
+        data = conn.recv(4096)
+        if not data: break
+        from_client += data
+        print(from_client)
+
+        conn.send("I am SERVER\n")
+
+    conn.close()
+    print("CLIENT DISCONNECTED")
 
